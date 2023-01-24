c3e7a1d6e1c77e8f38a0766c1fc58f86bb5fa201
Kunniii <nguyenairport56@gmail.com>
2023-01-24 19:55:04 +0700

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+# Using Socket
+
+# Listen on port 53
+
+# Learn about return data of a response
+
+# Receive a request, "understand it", and make a response
+
+# Detect OS for `host` file location.
+
+# Custom host file path as input
+
+# Using command line options
+
+# Wild card feature
+
+# Secodary DNS address.
+
+# ... ~_~ that's it.
